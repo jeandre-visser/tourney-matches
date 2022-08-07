bc6eaa178825a00262c0fbd2e5e48a22572bfa10
Jeandre <jeandrev1414@gmail.com>
2022-08-07 16:05:39 -0600

    Import helper functions

--- src/App.js
+++ src/App.js
@@ -3,15 +3,21 @@ import PlayerList from "./components/PlayerList";
 import MatchList from "./components/MatchList";
 import matchData from './data/MatchData';
 import playerData from './data/PlayerData';
+import { preparePlayerData, addWinsToPlayer } from './helpers/playerHelpers';
 
 function App(props) {
+
+  const playerDataArray = preparePlayerData(playerData);
+  const parsedPlayerData = addWinsToPlayer(playerDataArray, matchData);  
+
+
   return (
     <div className="App">
       <h1>
         Tourney Matches{" "}
         <span>Where Coding and Tournaments found their Match!</span>
       </h1>
-      <PlayerList matchData={matchData} playerData={playerData} />
+      <PlayerList matchData={matchData} playerData={parsedPlayerData} />
       <MatchList matchData={matchData} />
     </div>
   );
