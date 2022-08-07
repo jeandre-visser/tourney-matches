d9a4e62fbe71a22c22d501336c665e9b17ac3e0a
Jeandre <jeandrev1414@gmail.com>
2022-08-07 16:41:01 -0600

    Add += 1 to accumulator

--- src/helpers/playerHelpers.js
+++ src/helpers/playerHelpers.js
@@ -10,7 +10,7 @@ export const addWinsToPlayers = (playerDataArray, matchData) => {
     const currentWins = matchData.reduce((accumulator, match) => {
       //adds a win if the gamertag matches
       if (match.winner === player.gamerTag) {
-        return accumulator ++;
+        return accumulator += 1;
       } else {
         return accumulator;
       }
