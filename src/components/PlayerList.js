f302587bd6b0acfaa86ddc7c2e1e998e3feaa24c
Jeandre <jeandrev1414@gmail.com>
2022-08-07 15:35:06 -0600

    Pass in an individual player

--- src/components/PlayerList.js
+++ src/components/PlayerList.js
@@ -1,16 +1,17 @@
 import Player from "./Player";
-import playerData from '../data/playerData';
-import matchData from '../data/matchData';
+import playerData from '../data/PlayerData';
+import matchData from '../data/MatchData';
 import { preparePlayerData, addWinsToPlayer } from '../helpers/playerHelpers';
 
 function PlayerList(props) {
   const playerDataArray = preparePlayerData(playerData);
   const parsedPlayerData = addWinsToPlayer(playerDataArray, matchData);
+  const onePlayer = parsedPlayerData[0];
 
   return (  
     <section className="PlayerList">
     <h1>Current participating players</h1>
-    <Player />
+    <Player gamerTag={onePlayer.gamerTag} firstName={onePlayer.firstName} lastName={onePlayer.lastName} wins={onePlayer.wins} />
   </section>
   );
 }
